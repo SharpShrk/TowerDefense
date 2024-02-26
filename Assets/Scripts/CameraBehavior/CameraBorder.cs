86d36b5c65defb710a721e0a277a186e503671dd
ilyavorobyov <ilyavorobyov@outlook.com>
2024-02-26 17:43:07 +0300

    Добавил движение камеры
    движение камеры в пределах карты уровня

---
+++ Assets/Scripts/CameraBehavior/CameraBorder.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace CameraBehavior
+{
+    public class CameraBorder : MonoBehaviour
+    {
+    }
+}
---
+++ Assets/Scripts/CameraBehavior/CameraMover.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+namespace CameraBehavior
+{
+    public class CameraMover : MonoBehaviour
+    {
+        [SerializeField] private float _xPosition;
+        [SerializeField] private CameraBorder _leftBorder;
+        [SerializeField] private CameraBorder _rightBorder;
+
+        private float _speed = 47f;
+        private float _zOffset = 23f;
+        private Vector3 _origin;
+        private Vector3 _currentPosition;
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _camera = Camera.main;
+        }
+
+        private void LateUpdate()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                _origin = Input.mousePosition;
+                return;
+            }
+
+            if (!Input.GetMouseButton(0))
+            {
+                return;
+            }
+
+            _currentPosition = new Vector3(
+                _xPosition,
+                _camera.transform.position.y,
+                Mathf.Clamp(
+                    _camera.transform.position.z,
+                    _leftBorder.transform.position.z +
+                    _zOffset,
+                    _rightBorder.transform.position.z -
+                    _zOffset));
+            _camera.transform.position = _currentPosition;
+            Vector3 difference = _camera.ScreenToViewportPoint(Input.mousePosition - _origin);
+            Vector3 targetPosition = new Vector3(-(difference.y * _speed), 0, -(difference.x * _speed));
+            transform.Translate(targetPosition * Time.deltaTime, Space.World);
+        }
+    }
+}
