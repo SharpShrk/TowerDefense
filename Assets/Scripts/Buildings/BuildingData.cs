a43384433ca0398bc9134217b31eacdefe4f440d
Oleg <oleg0216@gmail.com>
2024-07-08 17:34:45 +0300

    add TurretData SO

--- Assets/Scripts/Buildings/BuildingData.cs
+++ Assets/Scripts/Buildings/BuildingData.cs
@@ -6,15 +6,18 @@ using UnityEngine.UI;
 
 public class BuildingData : MonoBehaviour
 {
-    [SerializeField] protected int _level;
-    [SerializeField] protected string _label;
-    //[SerializeField] private Image _icon;
-    [SerializeField] protected BuildType _type;
-    [SerializeField] protected GameObject _prefab;
+    [SerializeField] protected int Level;
+    [SerializeField] protected string Label;
+    //[SerializeField] private Image Icon;
+    [SerializeField] protected BuildType Type;
+    [SerializeField] protected GameObject Prefab;
 
-    public int Level => _level;
-    public string Label => _label;
-    public BuildType Type => _type;
-    public GameObject Prefab => _prefab;
-    //public Image Icon => _icon;
+    protected int MaxLevel = 3;
+
+    public int BuidlingLevel => Level;
+    public int BuidingMaxLevel => MaxLevel;
+    public string BuidlingLabel => Label;
+    public BuildType BuildingType => Type;
+    public GameObject BuildingPrefab => Prefab;
+    //public Image BuildingIcon => Icon;
 }
--- Assets/Scripts/Buildings/TurretData.cs
+++ Assets/Scripts/Buildings/TurretData.cs
@@ -1,16 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Agava.YandexGames.YandexGamesEnvironment;
 
 public class TurretData : BuildingData
 {
-    [SerializeField] private float _attackRange;
-    [SerializeField] private float _attackCooldown;
-    [SerializeField] private float _damage;
-    [SerializeField] private float _rotationSpeed;
+    [SerializeField] private TurretUpgradeData _upgradeData;
+
+    private float _attackRange;
+    private float _attackCooldown;
+    private float _damage;
+    private float _rotationSpeed;
 
     public float AttackRange => _attackRange;
     public float AttackCooldown => _attackCooldown;
     public float Damage => _damage;
     public float RotationSpeed => _rotationSpeed;
+
+    private void Start()
+    {
+        
+        ApplyUpgrade(BuidlingLevel);
+    }
+
+    public void LevelUp(int level)
+    {
+        if (level <= MaxLevel)
+        {
+            Level++;
+            ApplyUpgrade(level);
+        }
+    }
+
+    //тут проверить и убедиться, что данные точно назначаются в соответствии с уровнем
+    private void ApplyUpgrade(int level)
+    {
+        if (level <= _upgradeData.Levels.Length)
+        {
+            var upgradeLevelData = _upgradeData.Levels[level];
+            _attackRange = upgradeLevelData.AttackRange;
+            _attackCooldown = upgradeLevelData.AttackCooldown;
+            _damage = upgradeLevelData.Damage;
+            _rotationSpeed = upgradeLevelData.RotationSpeed;
+
+            Debug.Log("Уровень повышен до " + Level);
+        }
+        else
+        {
+            Debug.Log("Произошла какая то ошибка при апгрейде");
+        }
+    }
 }
