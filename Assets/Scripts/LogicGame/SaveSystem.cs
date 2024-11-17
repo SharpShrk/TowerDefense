50e3a69c751ab4329c2d31af3b9cea900645bc1d
Demon99999 <35568272+Demon99999@users.noreply.github.com>
2024-11-17 21:26:10 +0300

    Added record saving.
    Changed the script SaveSystem.cs and changed the currency panel.

--- Assets/Scripts/LogicGame/SaveSystem.cs
+++ Assets/Scripts/LogicGame/SaveSystem.cs
@@ -4,9 +4,13 @@ namespace GameLogic
 {
     public class SaveSystem : MonoBehaviour
     {
+        private const string AllScore = "AllScore";
         protected const string Level = "Level";
 
+        [SerializeField] private Score _score;
+
         private int _startNumberLevel = 1;
+        private int _startScore = 0;
         private int _zero;
 
         private void Start()
@@ -14,11 +18,22 @@ namespace GameLogic
             //ResetSave(); //Для тестов
         }
 
+        //public void LoadScore()
+        //{
+        //    if (PlayerPrefs.HasKey(Level))
+        //    {
+        //        if (_score != null)
+        //        {
+        //            _score.Init(PlayerPrefs.GetInt(AllScore));
+        //        }
+        //    }
+        //}
 
         public int LoadLevel()
         {
             if (PlayerPrefs.HasKey(Level))
             {
+                _score.Init(PlayerPrefs.GetInt(AllScore));
                 return PlayerPrefs.GetInt(Level);
             }
             
@@ -40,11 +55,13 @@ namespace GameLogic
         private void Save(int index)
         {
             PlayerPrefs.SetInt(Level, index);
+            PlayerPrefs.SetInt(AllScore, _score.AllScore);
         }
 
         private void ResetSave()
         {
             PlayerPrefs.SetInt(Level, _startNumberLevel);
+            PlayerPrefs.SetInt(AllScore, _startScore);
         }
     }
 }
